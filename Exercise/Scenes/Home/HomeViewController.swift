2564bd8c0f6c4148284fd41f23cc1283f7a1e95b
akirafabio <f.takahashi@ext.ppay.me>
2024-11-18 21:35:42 -0300

    ADD: TodoButton at HomeViewController

--- Exercise/Scenes/Home/HomeViewController.swift
+++ Exercise/Scenes/Home/HomeViewController.swift
@@ -6,13 +6,29 @@
 import UIKit
 
 final class HomeViewController: UIViewController {
-    var onAbout: (() -> Void) = {}
+    private var todoButtonAction: (() -> Void)?
+    private var aboutButtonAction: (() -> Void)?
 
-    private let button: UIButton = {
-        let button = UIButton()
-        button.setTitle(Strings.Home.aboutButtonTitle, for: .normal)
-        button.setTitleColor(.systemBlue, for: .normal)
-        return button
+    private let stackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.alignment = .center
+        return stackView
+    }()
+    private let todoButton: UIButton = {
+        let aboutButton = UIButton()
+        aboutButton.setTitle(Strings.Home.todoButtonTitle, for: .normal)
+        aboutButton.setTitleColor(.systemBlue, for: .normal)
+        aboutButton.backgroundColor = .secondarySystemBackground
+        return aboutButton
+    }()
+    private let aboutButton: UIButton = {
+        let aboutButton = UIButton()
+        aboutButton.setTitle(Strings.Home.aboutButtonTitle, for: .normal)
+        aboutButton.setTitleColor(.systemBlue, for: .normal)
+        aboutButton.backgroundColor = .secondarySystemBackground
+        return aboutButton
     }()
 
     override func viewDidLoad() {
@@ -21,25 +37,46 @@ final class HomeViewController: UIViewController {
     }
 
     @objc private func aboutButtonTap() {
-        onAbout()
+        aboutButtonAction?()
+    }
+
+    @objc private func todoButtonTap() {
+        todoButtonAction?()
+    }
+
+    @discardableResult
+    func onAboutButtonClick(_ action: (() -> Void)?) -> Self {
+        self.aboutButtonAction = action
+        return self
+    }
+
+    @discardableResult
+    func onTodoButtonClick(_ action: (() -> Void)?) -> Self {
+        self.todoButtonAction = action
+        return self
     }
 }
 
 extension HomeViewController: ViewConfiguration {
     func setupViewHierarchy() {
-        view.addSubview(button)
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(todoButton)
+        stackView.addArrangedSubview(aboutButton)
     }
 
     func setupViewConstraints() {
-        button.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
-        ])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
+            .isActive = true
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            .isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+            .isActive = true
     }
 
     func setupViewConfiguration() {
         view.backgroundColor = .white
-        button.addTarget(self, action: #selector(aboutButtonTap), for: .touchUpInside)
+        aboutButton.addTarget(self, action: #selector(aboutButtonTap), for: .touchUpInside)
+        todoButton.addTarget(self, action: #selector(todoButtonTap), for: .touchUpInside)
     }
 }
