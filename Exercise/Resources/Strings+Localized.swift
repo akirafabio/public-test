7857d9fd5ff7906dfe77081904a909ce3df0799b
akirafabio <f.takahashi@ext.ppay.me>
2024-11-20 21:38:41 -0300

    ADD: Localizable texts

--- Exercise/Resources/Strings+Localized.swift
+++ Exercise/Resources/Strings+Localized.swift
@@ -14,4 +14,20 @@ enum Strings {
         /// Public is where serious investors build their wealth. Grow your cash with higher yields and build a multi-asset portfolio for the long haul.
         static let label = String(localized: "About.label")
     }
+
+    enum Todo {
+        /// X
+        static let deleteSwipeActionTitle = String(localized: "Todo.DeleteSwipeAction.title")
+    }
+
+    enum TodoAlert {
+        /// Enter Task
+        static let textfieldPlaceholder = String(localized: "TodoAlert.Textfield.placeholder")
+        /// Cancel
+        static let cancelButtonTitle = String(localized: "TodoAlert.CancelButton.title")
+        /// Save
+        static let primaryButtonTitle = String(localized: "TodoAlert.PrimaryButton.title")
+        /// TO-DO
+        static let title = String(localized: "TodoAlert.title")
+    }
 }
